2900ac8df63f44cca1527f15294dc9814fa44dba
Kankit40 <Kankit40@gmail.com>
2022-07-06 00:49:14 +0530

    Attendence code

---
+++ Attendence.java
@@ -0,0 +1,15 @@
+public class Attendence {
+    public static void main(String[] args) {
+        
+
+        int a = (int)(Math.random() * 2);
+
+        if (a == 1){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+
+    }
+}
